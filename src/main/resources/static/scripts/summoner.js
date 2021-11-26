058d39cc87591ac46aba48975c5386603c4d1d24
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-26 19:38:35 +0100

    summoner.html and Js with queryString, URLParams & id under construction, not functional yet.

--- src/main/resources/static/scripts/summoner.js
+++ src/main/resources/static/scripts/summoner.js
@@ -1,8 +1,26 @@
 const queryString = window.location.search;
 const URLParams = new URLSearchParams(queryString);
-const summonerId = URLParams.get("summonerId");
+const name = URLParams.get("name");
 
 //TODO virker ikke endnu, der skal kigges på dette den skriver at summonerId = null
-fetch("http://localhost:8080/api/summoners/"+summonerId)
-.then(response => response.json())
-.then(summoner => console.log(summoner));
+fetch(baseURL + "/api/summoners/" + name)
+    .then(response => response.json())
+    .then(summoner => console.log(summoner));
+
+
+document.getElementById("avatar-icon").addEventListener("click", killAvatar);
+
+function killAvatar() {
+    document.getElementById("avatar-icon").style.visibility = "hidden";
+    document.getElementById("avatar-icon").style.opacity = "0";
+    document.getElementById("avatar-icon").style.transition = "visibility 0s 2s, opacity 1s linear";
+
+}
+
+function reviveAvatar() {
+    document.getElementById("avatar-icon").style.visibility = "visible";
+    document.getElementById("avatar-icon").style.opacity = "1";
+    document.getElementById("avatar-icon").style.transition = "opacity 1s linear";
+}
+
+setInterval(reviveAvatar, 5000)
