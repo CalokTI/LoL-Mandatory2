c48507d14c609b9564686c3cbbcb2410ecfb9f81
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-26 15:25:29 +0100

    fetch summoner & postSummoner function made really simple

--- src/main/java/kea/lolmandatory2/controllers/Matches.java
+++ src/main/java/kea/lolmandatory2/controllers/Matches.java
@@ -4,9 +4,9 @@ import kea.lolmandatory2.exceptions.ResourceNotFoundException;
 import kea.lolmandatory2.models.Match;
 import kea.lolmandatory2.repositories.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Matches {
@@ -15,7 +15,7 @@ public class Matches {
     MatchRepository matches;
 
     @GetMapping("/api/matches")
-    public Iterable<Match> getMatches() {
+    public List<Match> getMatches() {
         return matches.findAll();
     }
 
@@ -23,4 +23,37 @@ public class Matches {
     public Match getArtistByID(@PathVariable String id) {
         return matches.findById(id).orElseThrow(() -> (new ResourceNotFoundException("Match with id: " + id + " does not exist")));
     }
+
+    @PostMapping("/api/matches")
+    public Match addMatch(@RequestBody Match newMatch) {
+        newMatch.setId(null);
+        return matches.save(newMatch);
+    }
+
+    @PutMapping("/api/matches/{id}")
+    public String updateMatchById(@PathVariable String id, @RequestBody Match matchToUpdateWith) {
+        if (matches.existsById(id)) {
+            matchToUpdateWith.setId(id);
+            matches.save(matchToUpdateWith);
+            return "Match is now updated";
+        } else return "Match id is not found";
+    }
+
+    @PatchMapping("/api/matches/{id}")
+    public String patchMatchById(@PathVariable String id, @RequestBody Match newMatch) {
+        return matches.findById(id).map(foundMatch -> {
+            if (newMatch.getSummoners() == null) foundMatch.setSummoners(newMatch.getSummoners());
+            if (newMatch.getId() == null) foundMatch.setId(newMatch.getId());
+            matches.save(foundMatch);
+            return "Match Updated";
+        }).orElseThrow(() -> (new ResourceNotFoundException("Match with id: " + id + "Was not updated")));
+    }
+
+    @DeleteMapping("api/matches/{id}")
+    public String deleteMatchById(@PathVariable String id) {
+        matches.deleteById(id);
+        return "Match with id: " + id + "Has been deleted";
+    }
+
+
 }
--- src/main/resources/static/scripts/admin.js
+++ src/main/resources/static/scripts/admin.js
@@ -1,10 +1,27 @@
 document.getElementById("fetch-player").addEventListener("click", fetchSummoner)
 const key = "?api_key=RGAPI-d7474dfd-c5e4-40a7-96db-483c07a92085";
 
-function fetchSummoner() {
-    const name = document.getElementById("import-player-name").value;
+let newSummoner = {};
 
-    fetch("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + key)
-        .then(result => result.json())
-        .then(console.log)
+function fetchSummoner() {
+    const summonerName = document.getElementById("import-player-name").value;
+    fetch("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + key)
+        .then(response => response.json())
+        .then(result => {
+            newSummoner = result;
+            postSummoner();
+        })
+
 }
+
+function postSummoner() {
+    fetch("http://localhost:8080/api/summoners", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(newSummoner)
+    }).then(response => response.json());
+    console.log("Player added to DB")
+}
+
