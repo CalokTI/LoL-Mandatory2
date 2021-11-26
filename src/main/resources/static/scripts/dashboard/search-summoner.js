19fb7ec7d34c76882003a42c01fef879f2c3781d
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-26 23:11:02 +0100

    Fight Buttons works now, KillShot Made And Summoner card Lmao

--- src/main/resources/static/scripts/dashboard/search-summoner.js
+++ src/main/resources/static/scripts/dashboard/search-summoner.js
@@ -4,6 +4,7 @@ let name;
 
 // updates region
 document.getElementById("region").addEventListener("change", updateRegion)
+
 function updateRegion() {
     region = document.getElementById("region").value
     console.log(region)
@@ -11,8 +12,14 @@ function updateRegion() {
 
 }
 
-// updates name
+// updates name and can be used with the "Enter Key" instead of just clicking on a button.
 document.getElementById("search-input").addEventListener("keyup", updateName)
+document.getElementById("search-input").addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        window.location.href = document.getElementById("search-link").href;
+    }
+});
+
 function updateName() {
     name = document.getElementById("search-input").value
     console.log(name)
