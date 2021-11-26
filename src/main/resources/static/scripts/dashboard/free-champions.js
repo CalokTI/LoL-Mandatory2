17c8ca28ff0e8187fe0db5fe3a8caa345644b12f
Antonio-Banderas <anto8214@stud.kea.dk>
2021-11-26 22:30:02 +0100

    Icons for free champions on dashboard: ✅ entire project finished: ❌

--- src/main/resources/static/scripts/dashboard/free-champions.js
+++ src/main/resources/static/scripts/dashboard/free-champions.js
@@ -14,20 +14,31 @@ fetch(freeChampionsNA + "?api_key=" + apiKey)
         allFreeChampionsIds = freeChampionIdsForNewPlayers.concat(freeChampionIdsForAllPlayers)
         console.log(allFreeChampionsIds)
 
+        allFreeChampionsIds = [...new Set([...freeChampionIdsForNewPlayers,...freeChampionIdsForAllPlayers])]
+
         allFreeChampionsIds.map(championId => getChampion(championId))
     });
 
 function getChampion(championId) {
-    fetch(baseURL + "api/champions/" + championId)
+    console.log("you got id:" + championId)
+    fetch(baseURL + "/api/champions/" + championId)
         .then(respone => respone.json())
         .then(result => createChampionIcon(result))
 }
 
 
 const championsGallery = document.getElementById("free-champions");
-function createChampionIcon(champion){
 
-    innerhtml=
-        <a><image>champion.image</image></a>
+function createChampionIcon(champion) {
+    console.log("you got champion:" + champion.name)
+
+    const iconElement = document.createElement("a");
+    iconElement.href = "champion.html" + "?" + "name=" + champion.name;
+
+    iconElement.innerHTML = `
+    <img src="${escapeHTML(champion.image)}">
+    `
+
+    championsGallery.appendChild(iconElement);
 }
 
