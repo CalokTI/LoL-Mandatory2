a7048461418cf3c0892be0046b51e8fcaaf970ca
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-26 21:01:46 +0100

    summoner.html and Js with queryString, URLParams & id under construction, not functional yet.

--- src/main/resources/static/scripts/summoner.js
+++ src/main/resources/static/scripts/summoner.js
@@ -2,25 +2,56 @@ const queryString = window.location.search;
 const URLParams = new URLSearchParams(queryString);
 const name = URLParams.get("name");
 
-//TODO virker ikke endnu, der skal kigges på dette den skriver at summonerId = null
-fetch(baseURL + "/api/summoners/" + name)
+const summonerDivWrapper = document.getElementById("div-wrapper");
+
+fetch(baseURL + "/api/summoners/name/" + name)
     .then(response => response.json())
-    .then(summoner => console.log(summoner));
+    .then(summoner => {
+        console.log(summoner)
+        const summonerCard = document.createElement("div")
+        summonerCard.className = "player-card";
+
+        summonerCard.innerHTML = `
+    <div class="avatar-icon-div">
+            <img id="avatar-icon" class="avatar-icon" src="../static/images/TeemoSquare.webp"
+                 alt="character avatar image">
+                   <h3 id="summoner-name">${escapeHTML(summoner.name)}</h3>
+                   <p class="summoner-level">Level: ${escapeHTML(summoner.summonerLevel.toString())}</p>
+        </div>
+     
+         <div class="react-btn-div">
+            <button id="like-btn" class="react-btn">
+            <img class="react-img" src="../static/images/3044-pepe-clap.gif" alt="Like Button"></button>                                   
+            <i id="like-counter">${escapeHTML(summoner.friendly.toString())}</i>
+            <i id="dislike-counter">${escapeHTML(summoner.salty.toString())}</i>
+            <button id="dislike-btn" class="react-btn">
+            <img class="react-img" src="../static/images/SaltShake.gif" alt="Dislike Button"></button>                                                      
+        </div>
+        `
+        summonerDivWrapper.appendChild(summonerCard);
+        killShot();
+    });
 
 
-document.getElementById("avatar-icon").addEventListener("click", killAvatar);
+function killShot(){
 
-function killAvatar() {
-    document.getElementById("avatar-icon").style.visibility = "hidden";
-    document.getElementById("avatar-icon").style.opacity = "0";
-    document.getElementById("avatar-icon").style.transition = "visibility 0s 2s, opacity 1s linear";
+    document.getElementById("avatar-icon").addEventListener("click", killAvatar);
 
+    function killAvatar() {
+        document.getElementById("avatar-icon").style.visibility = "hidden";
+        document.getElementById("avatar-icon").style.opacity = "0";
+        document.getElementById("avatar-icon").style.transition = "visibility 0s 2s, opacity 1s linear";
+
+    }
+
+    function reviveAvatar() {
+        document.getElementById("avatar-icon").style.visibility = "visible";
+        document.getElementById("avatar-icon").style.opacity = "1";
+        document.getElementById("avatar-icon").style.transition = "opacity 1s linear";
+    }
+
+    setInterval(reviveAvatar, 5000)
 }
 
-function reviveAvatar() {
-    document.getElementById("avatar-icon").style.visibility = "visible";
-    document.getElementById("avatar-icon").style.opacity = "1";
-    document.getElementById("avatar-icon").style.transition = "opacity 1s linear";
-}
 
-setInterval(reviveAvatar, 5000)
+
