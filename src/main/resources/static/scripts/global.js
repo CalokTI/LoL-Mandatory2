040d95437d717c8c2298363d42d57aeae78bc72b
CalokTI <72436270+CalokTI@users.noreply.github.com>
2021-11-27 20:14:53 +0100

    Match stuff, could add more later

--- src/main/resources/static/scripts/global.js
+++ src/main/resources/static/scripts/global.js
@@ -4,7 +4,7 @@ const baseURL = "http://localhost:8080";
 
 // ---- RIOT API ---- //
 
-const apiKey = "RGAPI-32b7c7ee-809b-4705-9d4a-9ba1917bf77b";
+const apiKey = "";
 
 
 const freeChampionsNA = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations"
---
+++ src/main/resources/static/scripts/match.js
@@ -0,0 +1,100 @@
+const queryString = window.location.search;
+const URLParams = new URLSearchParams(queryString);
+const matchId = URLParams.get("matchId");
+
+
+fetch("https://americas.api.riotgames.com/lol/match/v5/matches/" + matchId + "?api_key=" + apiKey)
+    .then(response => response.json())
+    .then(result => {
+        createTeams(result.info);
+    })
+
+
+function createTeams(info) {
+    const matchSection = document.getElementById("match-section");
+    const blueTeamDiv = document.createElement("div");
+    blueTeamDiv.id = "match-team-blue";
+    blueTeamDiv.classList.add("teams");
+    matchSection.appendChild(blueTeamDiv);
+    const redTeamDiv = document.createElement("div");
+    redTeamDiv.id = "match-team-red";
+    redTeamDiv.classList.add("teams");
+    matchSection.appendChild(redTeamDiv);
+
+    if (info.teams[0].win) { //0 = blue
+        blueTeamDiv.classList.add("win");
+        redTeamDiv.classList.add("defeat");
+    } else {
+        blueTeamDiv.classList.add("defeat");
+        redTeamDiv.classList.add("win");
+    }
+
+    for (let i = 0; i < 5; i++) {
+        blueTeamDiv.appendChild(playerCard(info.participants[i]));
+    }
+
+    for (let i = 5; i < 10; i++) {
+        redTeamDiv.appendChild(playerCard(info.participants[i]));
+    }
+
+    /*
+    baron kills
+    dragon kills
+    tower kills
+    inhib kills
+    win/loss -> glow / shadow effect
+     */
+}
+
+function playerCard(participant) {
+    const kills = participant.kills;
+    const deaths = participant.deaths;
+    const assists = participant.assists;
+    const firstBlood = participant.firstBloodKill;
+    const firstTower = participant.firstTowerKill;
+
+    const playerDiv = document.createElement("div");
+    playerDiv.id = participant.puuid;
+    playerDiv.classList.add("card");
+
+    playerDiv.innerHTML = `
+        <img src="" alt="picture of ${participant.championName}"><br>
+        <strong></strong>
+        <hr>
+        <p>${kills} / ${deaths} / ${assists}</p>
+        <p>${participant.teamId}   ${participant.teamPosition}</p>
+        `
+
+    if (firstBlood) {
+        const firstBloodParagraph = document.createElement('p');
+        firstBloodParagraph.innerText = "🩸";
+        playerDiv.appendChild(firstBloodParagraph);
+    }
+    if (firstTower) {
+        const firstTowerParagraph = document.createElement('p');
+        firstTowerParagraph.innerText = "♟";
+        playerDiv.appendChild(firstTowerParagraph);
+    }
+
+    fetchChampionImage(participant.championId, participant.puuid);
+    fetchSummonerName(participant.puuid)
+    return playerDiv;
+
+}
+
+async function fetchChampionImage(championId, puuid) {
+    fetch(baseURL + "/api/champions/" + championId)
+        .then(response => response.json())
+        .then(champion => {
+            document.getElementById(puuid).children[0].src = escapeHTML(champion.image);
+        })
+}
+
+
+async function fetchSummonerName(puuid) {
+    fetch("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + puuid + "?api_key=" + apiKey)
+        .then(response => response.json())
+        .then(summoner => {
+            document.getElementById(puuid).children[2].textContent = escapeHTML(summoner.name);
+        })
+}
