356030b78351582ebba22ea5be70135612effe42
JuliusPanduro <73829015+JuliusPanduro@users.noreply.github.com>
2021-11-28 21:58:41 +0100

    Update global.js

--- src/main/resources/static/scripts/global.js
+++ src/main/resources/static/scripts/global.js
@@ -4,7 +4,7 @@ const baseURL = "http://localhost:8080";
 
 // ---- RIOT API ---- //
 
-const apiKey = "RGAPI-2e83abac-0d2e-4d31-89f1-c8450b09ffda";
+const apiKey = "";
 
 // AMERICA / NA
 const freeChampionsNA = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations"
