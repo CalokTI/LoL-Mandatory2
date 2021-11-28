789fc7c92e0609982ea29a181f6db8b4607be80b
Antonio-Banderas <anto8214@stud.kea.dk>
2021-11-28 20:40:08 +0100

    Added match history for a summoner

--- src/main/resources/static/scripts/global.js
+++ src/main/resources/static/scripts/global.js
@@ -6,5 +6,6 @@ const baseURL = "http://localhost:8080";
 
 const apiKey = "RGAPI-2e83abac-0d2e-4d31-89f1-c8450b09ffda";
 
-
+// AMERICA / NA
 const freeChampionsNA = "https://na1.api.riotgames.com/lol/platform/v3/champion-rotations"
+const matchHistoryAMERICAS = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"
